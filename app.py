c54c5b1af60e3d63ac809efb9f4c99fecf436592
Kudo <kudo@example.com>
2019-03-10 14:37:48 +0900

    合計を出力できるようにした

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from calculation import calculation_total
+
 if __name__ == '__main__':
     # ユーザーからの入力を受け取る
     input_data = input("データを入力して下さい(スペース区切り) > ")
@@ -5,7 +7,7 @@ if __name__ == '__main__':
     # print(input_data)
     # 文字列のリストに変換
     numbers_as_str = input_data.split(" ")
-    print(numbers_as_str)
+    # print(numbers_as_str)
 
     # 整数のリストに変換
     numbers = []  # リストを初期化
@@ -15,10 +17,14 @@ if __name__ == '__main__':
 
         numbers.append(int_num)  # numbersというリストに要素を追加
 
-    print(numbers)
+    #print(numbers)
 
     # 各統計量を計算する(合計，平均，)
-    # print("合計値は？？です")
-    # print("平均値は？？です")
+    total = calculation_total(numbers)
+
+
+
+
     # ユーザーに見やすいようにフォーマットする
     # 出力する
+    print(f"合計: {total}")
